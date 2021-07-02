de66ebd33c4ca43f084d9304c70b25402a639ebd
Jobin S <63976083+Jobin-S@users.noreply.github.com>
2021-07-02 07:46:23 +0530

    Update App.js

--- frontend/chat-app/src/App.js
+++ frontend/chat-app/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import { useState, useEffect } from 'react'
 import {io} from 'socket.io-client'
-const socket = io("http://localhost:4000");
+//const socket = io("http://localhost:4000");
+const socket = io("https://basic-chat-app-backend.herokuapp.com/")
 
 
 function App() {
